5c294a4e2c6968b3a9d34fadcf60a6a68c093baa
cecyoursnikol <nikol.cecyours@gmail.com>
2024-01-01 21:13:16 -0800

    Power of const

---
+++ C_Lec_123/const.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+	const int age = 15;
+	
+	const float Pi = 3.14;
+	
+	printf("The age is given : %i",age);
+	
+	printf("\nThe Value of PI is %.2f",Pi);
+	return 0;
+}
---
+++ priyanka/dawn of js/script.js
@@ -0,0 +1,14 @@
+
+document.getElementById('dr').innerHTML="The is js content";
+
+
+const a = 14;
+
+document.getElementById('r').innerHTML=a;
+
+function vara()
+{
+    const a = 12;
+    document.getElementById('v').innerHTML=a;
+}
+
