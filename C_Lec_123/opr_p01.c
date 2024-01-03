ba355908519ea260a6ef311bfce86c0066f56cd1
cecyoursnikol <nikol.cecyours@gmail.com>
2024-01-02 21:00:44 -0800

    opr

---
+++ C_Lec_123/opr_p01.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int a , b , c;
+	
+	printf("Enter the value of number 1:");
+	scanf("%d",&a);
+	printf("\nEnter the value of number 2:");
+	scanf("%i",&b);
+	
+	c = a + b;
+	
+	printf("The result is %d",c);
+	
+}
