da85bd0c2091a3d105242e36a814879a8e78ef45
cecyoursnikol <nikol.cecyours@gmail.com>
2024-01-04 21:14:03 -0800

    comapre

---
+++ C_Lec_123/compare.c
@@ -0,0 +1,11 @@
+//comparison opr
+#include<stdio.h>
+int main()
+{
+	int a = 10;
+	char s = 's';
+	
+	printf("The Correct ans : %d",a==10);
+	printf("\nThe correct ans of char : %c",s=='a');
+	return 0;
+}
