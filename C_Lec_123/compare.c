7f627ce6f0ddf5492d0f363959644015b953aff5
cecyoursnikol <nikol.cecyours@gmail.com>
2024-01-05 21:05:50 -0800

    k

--- C_Lec_123/compare.c
+++ C_Lec_123/compare.c
@@ -3,9 +3,7 @@
 int main()
 {
 	int a = 10;
-	char s = 's';
 	
 	printf("The Correct ans : %d",a==10);
-	printf("\nThe correct ans of char : %c",s=='a');
 	return 0;
 }
---
+++ C_Lec_123/not_equal.c
@@ -0,0 +1,8 @@
+//Not equal 
+
+#include<stdio.h>
+int main(){
+	int num = 10;
+	printf("Print True is value is not 10:%d",num!=11);
+	return 0;
+}
