b89b0016f98d3b44044199d108b8e035f53f7921
cecyoursnikol <nikol.cecyours@gmail.com>
2024-01-22 21:34:49 -0800

    Tisha For C

---
+++ Tisha_c_lang/Data_types.c
@@ -0,0 +1,8 @@
+#include<stdio.h>'
+main(){
+	int num = 10;
+	int NUM = 20;
+	printf("The Value of lower num %d",num);
+	printf("\nThe Value of Upper NUM %d",NUM);
+	
+}
---
+++ Tisha_c_lang/user_define.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	int number;
+	
+	printf("Eneter The Value u want :");
+	scanf("%d",&number);
+	printf("\nThe result is %i",number);
+}
