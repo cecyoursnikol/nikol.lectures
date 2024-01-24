4c723611190b491003feec93cac7d1ecb6aadc44
cecyoursnikol <nikol.cecyours@gmail.com>
2024-01-23 21:31:04 -0800

    data type

---
+++ C_Lec_123/floating.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	
+	float good;
+	
+	printf("Enter ur floating value (Kindly enter in point) :");
+	
+	scanf("%f",&good);
+	
+	printf("The value is : %f",good);
+	
+	return 0;
+}
