ce82b1bec58c3ad8448032f1f6613b5a7d7d1740
cecyoursnikol <nikol.cecyours@gmail.com>
2024-01-25 04:15:39 -0800

    opr

---
+++ Batch new/oprr.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	
+	int a , b , c;
+	
+	printf("Enter the value of A:");
+	scanf("%d",&a);
+	
+	printf("\nEnter the value of B:");
+	scanf("%d",&b);
+	
+	c = a+b;
+	
+	printf("\nThe result of add is %d",c);
+	
+	return 0;
+}
