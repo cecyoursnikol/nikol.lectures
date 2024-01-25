67d96f485565c70390d271246c0213d738b0a0e6
cecyoursnikol <nikol.cecyours@gmail.com>
2024-01-24 21:35:12 -0800

    new verse

---
+++ Tisha_c_lang/ch.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+	
+	char chr = 'r';
+	
+	printf("My new char is %c",chr);
+	return 0;
+}
---
+++ Tisha_c_lang/sessions_const.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+	
+	const	float PI = 3.14;
+	
+	const	int age_of_Tisha = 16;
+	
+//	age_of_Tisha = 18;
+
+	PI = 33.33;
+	
+//	printf("Set the age for Tisha :");
+//
+//	scanf("%d",&age_of_Tisha);
+//	
+	printf("The age u seted for Tisha is %d\n",age_of_Tisha);
+	
+	printf("Apda Pi ne value chhe %.2f",PI);
+	return 0;
+}
