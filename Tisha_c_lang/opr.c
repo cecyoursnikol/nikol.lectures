f61ef62bac486d95b58df1f4bd1336a018a17bab
cecyoursnikol <nikol.cecyours@gmail.com>
2024-01-26 21:31:58 -0800

    starting new opr

---
+++ Tisha_c_lang/opr.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+	
+	int x , y;
+	
+	printf("Enter the value of x y :");
+	scanf("%d %d",&x,&y);
+	
+//	printf("The value of X is %d & y is %d",x,y);
+	
+	printf("\nThe sum of x and y is %d",x+y);
+	
+	printf("\nThe sub of x and y is %d",x-y);
+	
+	return 0;
+}
