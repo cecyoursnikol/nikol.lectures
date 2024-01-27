3990319237d225140d4b2c7108cb05227c283de2
cecyoursnikol <nikol.cecyours@gmail.com>
2024-01-27 01:37:01 -0800

    opr

---
+++ arithmetic_add.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+	
+	int x , y , z;
+	
+	printf("enter the value of x");
+	scanf("%d",&x);
+	
+	printf("\nenter the value of y");
+	scanf("%d",&y);
+	
+	
+	z = x + y;
+	
+	printf("The result is %d",z);
+	
+	
+	return 0;
+}
