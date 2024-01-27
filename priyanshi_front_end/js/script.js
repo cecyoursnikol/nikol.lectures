060c5c00215138e3f4918721a877cafc3cf63087
cecyoursnikol <nikol.cecyours@gmail.com>
2024-01-26 20:45:56 -0800

    Js

---
+++ priyanshi_front_end/js/script.js
@@ -0,0 +1,5 @@
+document.getElementById('code').style.backgroundColor="red";
+document.getElementById('code').style.height="300px";
+document.getElementById('code').style.width="400px";
+document.getElementById('image').style.height="280px";
+document.getElementById('image').style.width="380px";
