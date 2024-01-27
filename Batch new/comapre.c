e594239c52000a5dd085103e79900fd8e641dfb5
cecyoursnikol <nikol.cecyours@gmail.com>
2024-01-27 04:28:38 -0800

    Comapare

---
+++ Batch new/comapre.c
@@ -0,0 +1,21 @@
+//compare 
+#include<stdio.h>
+int main(){
+	int a = 30, b = 20;
+	
+	
+	printf(" Equal to %d",a==b);	
+	
+	printf("\nNot equal to %d",a!=b);
+	
+	printf("\nGreater than %d",a>b);
+	
+	printf("\nless than %d",a<b);
+	
+	printf("\nGreater than eqal to %d",a>=b);
+	
+	printf("\nLess than eqal to %d",a<=b);
+	
+	
+	return 0;
+}
