b04c599cde1fb9ed0cfc01e9a333a88e300f7da6
cecyoursnikol <nikol.cecyours@gmail.com>
2024-01-26 22:03:25 -0800

    c lang

---
+++ Tisha_c_lang/incre.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+	
+	int a = 15;
+	
+	a--;  //post decrement
+	--a;  //pre decrement
+	
+	a++; //post increment 
+	++a; //pre increment
+
+	
+	printf("The value of a is %i",a);
+	
+	return 0;
+}
