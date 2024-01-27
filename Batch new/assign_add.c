b99647075bc7af0ea3007b5aa40e70b82bdba1ab
cecyoursnikol <nikol.cecyours@gmail.com>
2024-01-27 03:51:00 -0800

    assign opr

---
+++ Batch new/assign_add.c
@@ -0,0 +1,22 @@
+//assign opr
+
+#include<stdio.h>
+
+int main(){
+	
+	int x , y;
+	
+	printf("Enter the value of X and y :");
+	scanf("%d  %d",&x,&y);
+	
+	
+	printf("The result is %d %d",x,y);
+	
+	
+	
+	
+	printf("\nThe value with addtition assign opr is %d",x+=y);
+	
+	
+	return 0;
+}
