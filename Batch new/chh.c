7b51197b64579b1a6fbda82e37cf7449d2bcd970
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2024-01-31 04:14:12 -0800

    opr

---
+++ Batch new/chh.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+	
+	char a,c,d;
+	int b;	
+	printf("Enter the special symbol :");
+	scanf("%c %d %c %c",&a,&b,&c,&d);
+	
+	printf("------[%c][%d  ][%c][%c]",a,b,c,d);
+	return 0;
+}
