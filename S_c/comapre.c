2ffeffb5473649acdce5c0c66664042217a3316e
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2024-01-31 06:09:52 -0800

    opr

---
+++ S_c/comapre.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+	
+	int a, b;
+	
+	printf("Enter the value of a and b :");
+	scanf("%d %d",&a,&b);
+	
+	
+	printf("The result of eqault to is %d",a==b);   //eqal to
+	
+	
+	printf("\nThe result of not eqault to is %d",a!=b);   //not eqaul to 
+	
+	printf("\nThe result of grater than is %d",a>b);     //greater than
+	
+	printf("\nThe result of less than is %d",a<b);     //less than
+
+
+	printf("\nThe result of greater than eqaul is %d",a>=b);   //greater than equal 	
+	
+	printf("\nThe result of Less than eqaul is %d",a<=b);   //less than equal 
+	
+	return 0;
+}
