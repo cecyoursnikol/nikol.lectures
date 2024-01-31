19a8a90870a4cf6bbe6eca30f79003cb3e6bd600
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2024-01-31 06:23:04 -0800

    change

---
+++ S_c/log.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	
+	int num = 22;
+	
+	printf("Logical and %d",num<5 && num<50);			
+	
+	printf("\nLogical or %d",num = 20 || num > 20);
+	
+	printf("\nLogical not %d",!num==22);
+	return 0;
+}
