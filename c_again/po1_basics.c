a29daa556632346c8741fd46d23cf7c5dee5b647
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2024-02-11 21:15:36 -0800

    upload

---
+++ c_again/po1_basics.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+	
+	int num;	
+	printf("Enter the value :");
+	scanf("%d",&num);
+	
+	printf("%d",num);
+	
+	return 0;
+}
+
+//this is single line comment
+/*
+multi line comment
+
+*/
