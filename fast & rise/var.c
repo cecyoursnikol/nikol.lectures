5deb357e87288cb82ca5e0f62fc2e3dcb26ca7a6
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2024-02-16 06:27:16 -0800

    szjkdnvn

---
+++ fast & rise/var.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+	
+	int num = 450;
+	
+	printf("The value of num is %d",num);
+	
+	printf("\n%d",num);
+	printf("\n%d",num);printf("\n%d",num);printf("\n%d",num);
+	
+	
+	
+	return 0;
+}
