08f7b409b31f98511fb6aa7ecf5f6f956e31f854
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2024-02-19 05:57:31 -0800

    change

---
+++ fast & rise/formate.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+	
+	int a = 90;
+	printf("The value of a is %i",a);
+		
+	return 0;
+	
+}
---
+++ fast & rise/user_define.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+	
+//	user define value
+
+	int a;
+	
+	printf("Enter the value of a :");
+	scanf("%d",&a);
+	
+//	printing variable 
+
+	printf("The value of a is %i",a);
+	
+	return 0;
+}
