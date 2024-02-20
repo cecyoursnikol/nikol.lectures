2561ca8f9c2a501db10371d8379bc2082f23f694
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2024-02-20 05:15:23 -0800

    Change

---
+++ fast & rise/floot.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+	
+	float num = 45.55;   //pre define value 
+
+	double rohan;  // user define value 
+	
+	printf("The value of num is %.1f",num);
+	
+	printf("\nEnter a grade for rohan :");
+	scanf("%lf",&rohan);
+	
+	printf("The grade u given for Rohan is %.2lf",rohan);
+	
+	return 0;
+}
