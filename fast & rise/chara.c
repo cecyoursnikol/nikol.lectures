cc655db41849ff4cd5d0b6a2a23356d092a857f0
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2024-02-20 05:48:54 -0800

    change

---
+++ fast & rise/chara.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+	
+	char gender;
+	
+	printf("Enter m or f :");
+	scanf("%c",&gender);
+	
+	printf("Ur Gender is %c",gender);
+	
+	
+	return 0;
+}
---
+++ fast & rise/fix.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+	
+	const float PI =3.14;
+	
+	printf("The value of PI is %.2f",PI);
+	
+	return 0;
+}
