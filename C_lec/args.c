d0a4adb0e005cc8bb296d60bb90f61870d25c6b3
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2024-02-21 05:33:42 -0800

    change

---
+++ C_lec/args.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int func(int a){
+	
+	printf("Enter the value :");
+	scanf("%d",&a);
+	
+	
+	printf("\nTHe value is %d",a);
+	return 0;
+}
+
+
+int main(){
+	
+	func(5);
+	
+	
+	return 0;
+}
