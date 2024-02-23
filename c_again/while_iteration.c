72ddbe400b8139519090c74b0380a18898454dc1
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2024-02-22 19:52:36 -0800

    Loops

---
+++ c_again/while_iteration.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+int main(){
+	
+	int i;
+	
+	i = 1;
+	
+	printf("\n-----------------------While Loop");
+
+	while(i<=5){
+		printf("\n%i",i);
+		i++;
+	}
+	
+	
+	printf("\n-----------------------do While Loop");
+	i=1;
+	do
+	{
+		printf("\n%i",i);
+		i++;
+	}while(i<=5);
+	
+	printf("\n-----------------------For Loop");
+	i = 1;
+	for(i=1;i<=5;i++)
+	{
+		printf("\n%i",i);
+	}
+	
+	return 0;
+}
+
+
+
+
