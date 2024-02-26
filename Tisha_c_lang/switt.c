5692fb4aedeae15b40d4c29e1d6350dc26b2b614
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2024-02-25 20:56:36 -0800

    Switch

---
+++ Tisha_c_lang/switt.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+
+
+int main()
+{
+	int day;
+	
+	printf("Enter the number of day :");
+	scanf("%d",&day);
+	
+	switch(day){
+		case 1:
+			{
+				printf("Sunday");
+				break;
+			}
+		case 2:
+			{
+				printf("MOn");
+				break;
+			}
+		case 3:
+			{
+				printf("Tue");
+				break;
+			}
+		case 4:
+			{
+				printf("Wed");
+				break;
+			}
+		case 5:
+			{
+				printf("Thur");
+				break;
+			}
+		case 6:
+			{
+				printf("Fri");
+				break;
+			}
+		case 7:
+			{
+				printf("Sat");
+				break;
+			}
+			
+		default:
+			{
+				printf("Eneter only days");
+			}
+	}
+	return 0;
+}
---
+++ Tisha_c_lang/ternarery.c
@@ -0,0 +1,14 @@
+
+//ternary opr
+
+#include<stdio.h>
+
+int main()
+{
+	
+	int a = 4;
+	
+	
+	(a>15)?printf("Yes a is greater than 15"):printf("Noo a is smaller than 15");
+	return 0;
+}
