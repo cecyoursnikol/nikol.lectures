9e080b36d6ec860aafe5479f291a55efcab628d5
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2024-03-07 05:07:32 -0800

    Condition statment

---
+++ fast & rise/if_statment.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+{
+	
+	char gender;
+	
+	printf("Enter m if u are Male  :");
+	scanf("%c",&gender);
+	
+	if(gender == 'm')
+	{
+//		statment
+
+	printf("U are Male");
+	}
+	else
+	{
+//		statment
+
+	printf("U are not male");
+	}
+	
+	return 0;
+}
