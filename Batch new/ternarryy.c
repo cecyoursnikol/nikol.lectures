636dd2dceaf1fe9f826afea2fd18d1911dc20de0
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2024-03-13 05:16:29 -0700

    flow to go

---
+++ Batch new/ternarryy.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+	int num;
+	
+	printf("Eneter the value above 20:");
+	scanf("%d",&num);
+	
+	(num>20) ? printf("Yes u are correct number is greater than 20") : printf("error");
+	return 0;
+}
