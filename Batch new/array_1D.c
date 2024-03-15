13ff47ec5580a26b38882f7f985ec83952ab6d61
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2024-03-15 05:17:19 -0700

    1d array

---
+++ Batch new/array_1D.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+	
+	int a[7] = {56,33,46,32,78,90,323};
+//				0, 1, 2, 3, 4,  5,  6
+	int i;
+	
+//	printf("%d",a[3]);
+//	printf("\n%d",a[4]);
+//	printf("\n%d",a[6]);
+
+for(i=0;i<7;i++){
+	
+	printf("%d\n",a[i]);
+}
+	return 0;
+}
