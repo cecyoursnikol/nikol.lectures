3a894272708bfcf8f820cc644dce286d5baa3807
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2024-03-18 05:37:27 -0700

    Var

---
+++ python-123/P02_comments.py
@@ -0,0 +1,10 @@
+print("Hello Yash")
+
+# print('jlzndvj')
+
+
+# aa print nu func chhe
+"""
+print("Single line Comment")
+print("multi line comment")
+"""
---
+++ python-123/P03_variables.py
@@ -0,0 +1,14 @@
+# print(45)
+
+num = 45
+NUM = 85
+name = "Rohan"
+
+
+print(num)
+print(name)
+print(NUM)
+
+print(type(num))
+
+print(type(name))
