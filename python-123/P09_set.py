fd0f3acbe40e8e0b10988f3e0c1007fc043b4d9f
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2024-03-21 05:56:15 -0700

    Set

---
+++ python-123/P09_set.py
@@ -0,0 +1,17 @@
+item = {"fruits" , "veges" , "living things"}
+
+print(item)
+print(type(item))
+
+# item[2] = "person"
+
+print(item)
+
+# item.remove("veges")
+print(item)
+
+item.add("vansh")
+print(item)
+
+for i in item:
+    print(i)
