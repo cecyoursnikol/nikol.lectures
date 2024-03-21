e99931c24cecb95d680436d02a139ef3d4329ea1
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2024-03-21 05:40:25 -0700

    Uplod

---
+++ Batch new/stringg.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+	
+	char arr[50] = {"HEllo "};
+	int i;
+	
+//	printf("Enter the value is Word :");
+//	scanf("%s",arr);
+	
+//	for(i = 0; i < 1;i++)
+	printf("%s",arr);
+	
+	return 0;
+}
---
+++ python-123/P08_tuple.py
@@ -0,0 +1,17 @@
+car = ("BMw","Farrare","Mustang")
+carlist = ["me","manav","Jay"]
+
+
+print(car)
+
+print(type(car))
+# print(type(carlist))
+
+# print(carlist[1])
+# carlist[1] = "Yash"
+
+carlist[1] = "Yash"
+
+
+
+print(carlist)
