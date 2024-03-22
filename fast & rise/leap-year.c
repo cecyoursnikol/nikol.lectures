28f3a1b1b3ef877a7a85bb12b35b654c04bd3551
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2024-03-21 21:58:44 -0700

    BLock

---
+++ fast & rise/leap-year.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+   int year,y;
+
+   printf("Enter the Year :\n");
+   scanf("%d",&year);
+
+ y = year % 400 == 0 || year % 100 == 0 || year % 4 == 0;
+
+ switch (y) {
+        case 1:
+            printf("\n%d is the leap year.\n", year);
+            break;
+
+        case 0:
+            printf("\n%d is not the leap year.\n", year);
+            break;
+
+        default:
+            printf("\n%d is not the leap year.\n", year);
+    }
+
+}
