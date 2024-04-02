d4e806a24bcd548acdd1cb4852e4d370ce14f6f0
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2024-04-02 06:03:24 -0700

    Func Start

---
+++ fast & rise/func.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int rohan(int a){
+	
+	a = 100;
+	
+	
+	printf("Hello I am Rohan\n");
+	
+	printf("The value of my A is : %d\n",a);
+	return 0;
+}
+
+int main(){
+	
+	rohan(1);
+	rohan(2);
+	rohan(3);
+	rohan(4);
+	rohan(5);
+
+	
+	return 0;
+}
