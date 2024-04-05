a432c0beb82f1553e8dcf7696e48d55dd2c119ad
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2024-04-05 05:28:43 -0700

    conditional statments

---
+++ define.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+#define alok 100
+
+
+#undef alok
+int main(){
+	
+	printf("%d",alok);
+	
+	return 0;
+}
---
+++ python-123/P17_conditional_if_else.py
@@ -0,0 +1,14 @@
+num  = 10
+
+num1 = 2000
+
+num2 = 100
+
+
+if(num>num1 and num>num2):
+    print("num is greater")
+    
+elif(num2>num and num2>num1):
+    print("num2 is greater")
+else:
+    print("num1 is greater")
