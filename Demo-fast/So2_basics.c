27f1326849e5c6a3ce13e3ad866595a5bd148dda
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2024-04-22 02:23:31 -0700

    Basics

---
+++ Demo-fast/So2_basics.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+	
+	int num = 15;
+	
+	int num1 = 100;
+	
+	int result;
+	
+	result = num + num1;
+	
+	printf("The value of total num is %d",result);
+	
+	return 0;
+}
