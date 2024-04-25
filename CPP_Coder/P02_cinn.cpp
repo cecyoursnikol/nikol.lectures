3af5c2b1e246209da5d8bb63c92a1f1c1eaee5d6
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2024-04-24 21:09:10 -0700

    All upload

---
+++ CPP_Coder/P02_cinn.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int age;
+    cout<<"Enter ur age";
+    cin>>age;
+
+    cout<<"Ur age is "<<age;
+
+}
---
+++ python-123/p_19_flow-control.py
@@ -0,0 +1,4 @@
+x = 10
+for x in range(x > 10):
+ print(x)
+x=+1
