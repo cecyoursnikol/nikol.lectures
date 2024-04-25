0c9d3b5f0c920e0828baf81fa8a621ee3d33f9d9
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2024-04-25 02:04:12 -0700

    data Types

---
+++ C_lang_quick/intro-with-dataTypes.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+	
+	int kirti = 10;
+	
+	float f = 3.14;
+	
+	char name = 'r';
+	
+	double num = 1234;
+	
+	printf("The value is %i",kirti);
+	
+	printf("\nThe value of float is %.2f",f);
+	
+	printf("\nThe value of chat is %c",name);
+	
+	printf("\nThe value of double is %.2lf",num);
+	
+	return 0;
+}
