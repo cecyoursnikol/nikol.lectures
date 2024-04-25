07b810cb2f5156a723de38d7b16cfef5eb5656f8
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2024-04-25 02:54:16 -0700

    const and comments

---
+++ C_lang_quick/consst.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+	
+	
+	const int a = 12;
+	
+	printf("The default value of A is %d",a);
+	
+	a = 23;
+	
+	
+	printf("\nThe new value for a is %d",a);
+	return 0;
+}
