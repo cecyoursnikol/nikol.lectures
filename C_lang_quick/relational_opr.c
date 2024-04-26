65671291667ca2a3c2cb9f43b8376e90c2ab4968
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2024-04-26 03:16:15 -0700

    relational opr

---
+++ C_lang_quick/relational_opr.c
@@ -0,0 +1,32 @@
+//relational opr
+
+//always ans gives in bool values
+#include<stdio.h>
+
+int main(){
+	
+	int a , b;
+	
+	a = 100;
+	b = 10;
+//	equal to opr
+	printf("The equal to ans is %d",a==b);
+	
+//	not equal 
+	printf("\nThe not equal to ans is %d",a!=b);
+	
+//	greater than
+	printf("\nThe Greater  ans is %d",a>b);
+	
+	//	smaller than
+	printf("\nThe smaller  ans is %d",a<b);
+	
+	//	greater than equal
+	printf("\nThe Greater  ans is %d",a>=b);
+	
+	//	smaller than equal
+	printf("\nThe smaller  ans is %d",a<=b);
+
+	
+	return 0;
+}
