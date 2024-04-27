6f1c13d008f22dde3231228e4657320331c94a6e
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2024-04-27 03:53:50 -0700

    increment decrement

---
+++ C_lang_quick/incre-decre.c
@@ -0,0 +1,28 @@
+//increment decrement
+
+#include<stdio.h>
+
+int main(){
+	
+	int num = 10;
+	
+	printf("The simple ans is %d",num);
+	
+//	num++;
+//	++num;
+	
+	printf("\n\nNow incremented num is %d",num);
+	
+	num--;
+	num--;
+	
+//	num == 10
+	printf("\n\nNow decremented num is %d",num);
+	
+	//post increment  num++;
+//		pre increment ++num;
+//	post decerement   num--;
+//	pre decrement	  --num;
+	
+	return 0;
+}
