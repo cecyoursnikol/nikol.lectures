cd9f31a78bcade684b69fc8305d43507fa910ec7
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2024-04-27 02:22:00 -0700

    logival opr

---
+++ C_lang_quick/logical_opr.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+int main(){
+	
+	char name;
+	
+	int num;
+	
+//	logical and
+	
+//	printf("Enter a letter (a) :");
+//	scanf("%c",&name);
+//	
+//	printf("enter a num 10 :");
+//	scanf("%d",&num);
+//	
+
+//logical or ||
+//	
+//	printf("write atleast one correct\n");
+//	printf("Enter a letter (a) :");
+//	scanf("%c",&name);
+//	
+//	printf("enter a num 10 :");
+//	scanf("%d",&num);
+	
+	
+//	LOGICAL not
+
+	name = 'f';
+	printf("%d",!(name == 'a'));
+	
+//	opr input
+//	printf("U have mind : %d",name == 'a' || num == 10);
+	
+	return 0;
+}
