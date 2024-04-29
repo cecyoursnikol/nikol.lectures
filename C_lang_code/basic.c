47754c13c2f520dcb8299d9601aa384785dac769
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2024-04-29 04:42:30 -0700

    basic

---
+++ C_lang_code/basic.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+	
+	int number; //variable declaration
+	
+	number = 50;  // variable inti
+	
+	printf("This  is how i told %d", number);
+	return 0;
+}
--- C_lang_quick/ladder_if.c
+++ C_lang_quick/ladder_if.c
@@ -1,3 +1,4 @@
+//ladder if
 #include<stdio.h>
 
 int main(){
@@ -7,11 +8,11 @@ int main(){
 	printf("Enter ur Gender :");
 	scanf("%c",&gender);
 	
-	if(gender == 'm'){
+	if(gender == 'm'){  // firsr cond
 		
 		printf("Ur gender is Male");
 	}
-	else if(gender == 'f'){
+	else if(gender == 'f'){   //second cond
 		printf("ur gender is female");
 	}
 	else{
