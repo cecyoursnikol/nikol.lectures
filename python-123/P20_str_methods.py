726d4c2711a6fee05efd1cbfaf9130f91f288c75
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2024-05-03 04:55:23 -0700

    py

---
+++ python-123/P20_str_methods.py
@@ -0,0 +1,22 @@
+str = "tHis is random string"
+
+result = str.capitalize()
+
+
+str1 = "Good Bye" 
+
+result = str1.lower()
+
+str2 = "hello boy"
+
+result = str2.upper()
+
+str3 = "            This is future        "
+
+result = str3.split()
+
+str4 = "            This is future        "
+
+result = str4.strip()
+
+print(result)
