6574911adab707ec1ea37f6d0aae9a073f89b4d6
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2024-05-09 02:22:08 -0700

    gvhjkl

---
+++ C_lang_quick/array_start.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+	
+	int arr[5] = {10,20,30,40,50};
+//				  0 ,1, 2, 3, 4
+
+	int i;	
+	
+//	printf("The value is %d",arr[4]);
+
+	for(i = 0;  i < 5; i++){
+		
+		printf("%d\n",arr[i]);
+	}
+	
+	return 0;
+}
