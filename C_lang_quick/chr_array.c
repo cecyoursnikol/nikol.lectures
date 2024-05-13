42439088e316fc7efc4fe05701c44681d265579a
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2024-05-13 04:06:08 -0700

    str str

---
+++ C_lang_quick/chr_array.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+	
+	char str[3] = {'a','b','c'};
+	
+	int i;
+	for(i = 0; i < 3; i++){
+		
+		printf("%c",str[i]);
+	}
+	
+	return 0;
+}
---
+++ C_lang_quick/strr.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+	
+	char str[] = "Hello Kirti how are u ??";
+	
+	int i;
+	
+	printf("%s\n",str);
+	
+	for(i = 0; i < 10; i++){
+		
+		printf("%c",str[i]);
+	}
+	
+	
+	return 0;
+}
