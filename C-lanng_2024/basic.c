8bb57f861bb8f90a4dba103c25d5bd827aa846c0
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2024-05-13 05:08:50 -0700

    Basic

---
+++ C-lanng_2024/basic.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+	
+	//THis is for only learn 
+	//szfhszdf
+	
+	/*
+	
+	
+	zdsfg
+	
+	sdffg
+	dsfg
+	sdfh
+	sdtty
+	sefg*/
+	printf("Hello World");
+	
+	return 0;
+}
---
+++ C-lanng_2024/input.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+	
+	int num , num1 , res;
+	
+	
+	printf("Enter the value in num :");
+	scanf("%d",&num);
+	
+	printf("\nEnter the value in num1 :");
+	scanf("%d",&num1);
+	
+	
+	res = num +  num1;
+	printf("The add is %d",res);
+	return 0;
+}
