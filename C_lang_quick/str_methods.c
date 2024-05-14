747a6515dc78bd5f43b8cef40b17ee4952dcdbce
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2024-05-14 02:35:36 -0700

    str methods

---
+++ C_lang_quick/str_methods.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+	
+	char	str[] = "The VALUE of code";
+	
+	char str1[] = "is too precious";
+	
+	char strstr[20];
+	
+//	printf("%s",str);
+//	printf("%s",str1);
+
+//	strcat(str,str1);   //strcat
+//	printf("%s",str);
+//	
+	
+//	printf("%d",strlen(str));	
+//	
+//	
+////	strlwr
+//	printf("\n%s",strlwr(str));
+//	
+////	strupr
+//	printf("\n%s",strupr(str));
+
+strcpy(strstr,str1);
+
+printf("%s",strstr);
+
+	
+	return 0;
+}
