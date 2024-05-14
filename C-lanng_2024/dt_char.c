141d0d2acf796800e079be593e4d9b4a2210b92c
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2024-05-14 05:28:22 -0700

    data

---
+++ C-lanng_2024/dt_char.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+void main(){
+	
+	char xlcbnbn = 'm';
+	
+	printf("The person is %c",xlcbnbn);
+}
---
+++ C-lanng_2024/dt_double.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+	
+	double num;
+	
+	printf("enter value in double :");
+	scanf("%lf",&num);
+	
+	printf("The ans is %lf",num);
+	
+	return 0;
+}
