5215c11a4988ed1c57110e16e2ba7817292828b7
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2024-05-14 05:34:19 -0700

    Array

---
+++ Half_begain_c/array_start.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+	
+	int arr[5] = {1,2,3,4,5};
+//				  0,1,2,3,4
+
+	int arr1[5] = {10,20,30,40,50};
+	int i;	
+	
+//	printf("The ans is = %d",arr[1]);
+//	for(i = 0 ; i < 5 ; i++){
+//		
+//		printf("%d",arr[i]);
+//	}
+	
+	for(i = 0 ; i < 5 ; i++){
+		
+		printf("\n%d",arr1[i]);
+	}
+}
