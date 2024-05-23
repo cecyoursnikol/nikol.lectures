16cad07e3975a70a298b256940e2660fd006c6b2
cecyoursnikol <nikol.cecyours@gmail.com>
2024-05-23 08:46:05 -0400

    23/05/2024⠀⎦⎡⠀08:46⠀⎦⎢⠀Thursday

---
+++ Cpp_456/Oop_basic.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+class firstClass{
+    public:
+
+    int a , b;
+};
+
+int main(){
+
+    firstClass obj;
+
+    obj.a  = 545;
+
+    cout<<"THis  is the ans "<<obj.a;
+    return 0;
+}
