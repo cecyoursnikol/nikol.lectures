4cda60f6aa1c20b1003646fd9e82cd711d43b097
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2024-06-07 05:33:35 -0700

    con

---
+++ Cpp_981/constructor_cpp.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+	class Students{
+		public:
+			//declaring constructor
+			
+			int name(){
+				
+			cout<<"\nU are fool";
+			}
+			
+			Students(){
+				
+				cout<<"I am magical constructor!!";
+			}
+		
+	};
+
+int main(){
+	
+	Students aditya;
+	aditya.name();
+	return 0;
+}
