730f10636241d411b540df5791526cd8c6b6e60f
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2024-06-21 05:15:33 -0700

    uplload

---
+++ C-lanng_2024/str.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+	
+	char str[] = "Hello";
+	int i;
+	
+//	printf("%s",str);
+
+
+	for(i = 0; i < 5; i++){
+		
+		printf("%c",str[i]);
+	}
+	return 0;
+}
---
+++ Node-start/write_file.js
@@ -0,0 +1,5 @@
+const fs = require("fs");
+
+fs.writeFile("hello.txt", "THis is the file content",(err)=>{
+    console.log("hszdbv");  
+})
