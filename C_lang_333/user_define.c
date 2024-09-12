cf44f6e62dc3add5065c4571ae62fb9dc5b41ff9
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2024-09-12 05:41:02 -0700

    user

---
+++ C_lang_333/user_define.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main(){
+	
+	int num;
+	
+	
+	
+	//This is for user define value 
+	/*
+	xdgnh
+	xg
+	cfhj
+	cghm
+	cghmm
+	
+	xdfh*/
+	
+	printf("Enter any number :");
+	scanf("%d",&num);
+	
+	
+	printf("The inputed value is %d",num);
+	
+	return 0;
+}
