ac5d9d9d8ab9a9ed6b5bf8c8246e6f6710e7e447
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2024-09-13 05:26:17 -0700

    upload data types

---
+++ C_lang_333/char-dt.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+	
+	char ch = a;
+	
+	
+//	printf("Enter any alpha :");
+//	scanf("%c",&ch);
+	
+	
+	printf("the ans is %c",ch);
+	
+	return 0;
+}
---
+++ C_lang_333/data-type-float.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+	
+	
+	double b;
+	
+	float a = 34.6;
+	
+	
+	
+	
+//	printf("%.2f",a);
+
+	printf("Enter Double Value :");
+	scanf("%lf",&b);
+	
+	printf("%.2lf",b);
+	
+	
+	return 0;
+}
