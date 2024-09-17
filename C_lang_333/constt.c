8dcdcd6823ef5d738b1ac365b762567cc0ad1270
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2024-09-17 05:34:58 -0700

    upload

---
+++ C_lang_333/constt.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+	
+	const int num = 37;
+	
+	
+	printf("The Current value is %d",num);
+	
+	num = 89;    //updated value 
+	
+	
+	printf("\nThe Updated value is %d",num);
+	
+	return 0;
+}
---
+++ C_lang_333/opr-artith.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+	
+	int c, a = 9,b = 5;
+	
+	c = a+b;
+	
+	printf("Add is %d",c);
+	
+	return 0;
+}
