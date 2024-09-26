804d1ba5b2456a825050355a92083050a1cc9938
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2024-09-25 21:30:50 -0700

    While Loop

---
+++ C-255/increment-decrement.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+	
+	int num;
+	
+	
+	num = 10;
+	
+//	increment
+	num++;  //post
+	
+	++num;  //pre
+	printf("THe num is %d",num);
+	
+	num--; //post
+	
+	--num; //pre
+	printf("\nTHe num is %d",num);
+	
+	return 0;
+}
---
+++ C-255/while-loop.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+	
+	int i;
+	
+	while(i<10){
+		
+		printf(" HEllo world ! %d\n",i);
+		i++;
+	}
+	
+	return 0;
+}
