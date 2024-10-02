d622c9d3ab17fcc300246102e631cf437c926e13
cecyoursnikol <nikol.cecyours@gmail.com>
2024-10-02 13:50:25 +0530

    update

---
+++ C-255/array-start.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+	
+//	array
+
+	int num[5] = {441,552,43,434,545};
+//				  0,1,2,3,4
+	int i;
+	
+	printf("array is %d\n",num[4]);
+	
+	
+	for(i = 0; i < 5; i++ ){
+	
+		printf("%d \n",num[i]);
+	} 
+		
+	
+	return 0;
+}
