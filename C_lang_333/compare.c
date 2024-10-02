572fc5fa8304ff34d57e988adebe01e7ddd41bdc
cecyoursnikol <nikol.cecyours@gmail.com>
2024-10-02 19:24:10 +0530

    upload

---
+++ C_lang_333/compare.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(){
+	
+	
+	int num = 50;
+	
+	// 35 == 90
+	
+	printf("%d",num);
+	printf("Equal %d",num == 90);
+	
+	
+	printf("\nNot Equal %d",num != 80);
+	
+	
+	printf("\n Greater than  %d",num > 50);
+	
+	printf("\n Smaller than  %d",num < 50);
+	
+	printf("\n Greater than equal %d",num >= 50);
+	
+	printf("\n Smaller than equal %d",num <= 50);
+	return 0;
+}
---
+++ C_lang_333/rev-part-1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+	
+	const	int val = 45;
+	
+	
+	printf("%d",val);
+	
+	
+	val = 566;
+	
+	printf("\n%d",val);
+	
+	return 0;
+}
