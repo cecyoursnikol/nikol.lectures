257c5ec2416f9902664df31c4920b0767b2b11d7
cecyoursnikol <nikol.cecyours@gmail.com>
2024-11-25 14:15:18 +0530

    String Update

---
+++ C-255/string.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+	
+	
+	char str[] = "Hello";
+	
+	char str2[] = "Hello";
+	
+	
+	char str3[10];
+	
+//	
+//	printf("%s",str);
+//	
+//	printf("\nLength Of string is %d",strlen(str)); 
+//	
+//	
+//	strcat(str,str2);
+//	
+//	printf("\n%s ",str);
+//	
+//	
+//	strcpy(str3,str);
+//	
+//	printf("\n The Str3 is %s",str3);
+	
+	
+//	printf("\n%d",strcmp(str,str2));
+
+
+printf("%s",strupr(str2));
+
+printf("\n%s",strlwr(str2));
+	return 0;
+}
