e7b4fe23ad396dd2761a87d2480c0736bb286960
cecyoursnikol <nikol.cecyours@gmail.com>
2024-11-26 11:16:44 +0530

    struct union

---
+++ C-255/structure-union.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+union Mystruct{
+	
+	
+	int num;
+}s1,s2;
+
+
+int main(){
+
+	;	
+	printf("enter the value in struct Member num :");	
+	scanf("%d",&s1.num);
+	
+	
+	
+	printf("Ans is %d",s1.num);
+	
+	
+	
+	printf("\nenter the value in struct Member num :");	
+	scanf("%d",&s2.num);
+	
+	
+	
+	printf("Ans is %d",s2.num);
+	
+	
+	
+	return 0;
+}
---
+++ C_lang_333/structure-union.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+struct{
+	
+	
+	int num;
+}s1;
+
+
+int main(){
+	
+	printf("enter the value in struct Member num :");	
+	scanf("%d",%s1.num);
+	
+	
+	
+	printf("Ans is %d",s1.num);
+	
+	return 0;
+}
