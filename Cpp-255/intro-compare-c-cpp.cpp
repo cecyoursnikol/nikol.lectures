a5d2b4daaafeb6c3cd26ffb1a7c2951c1c6bdaf9
cecyoursnikol <nikol.cecyours@gmail.com>
2024-12-10 14:10:14 +0530

    Start

---
+++ Cpp-255/intro-compare-c-cpp.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	int num;
+	
+	printf("This is C\n");
+	
+	cout<<"This is Good in CPP\n"; 
+	
+	scanf("%d",&num);
+	
+	cin>>num;
+	
+	cout<<endl<<num;
+	
+	return 0;
+}
