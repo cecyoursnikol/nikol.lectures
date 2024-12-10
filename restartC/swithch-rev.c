b1dd94763b017a5bd6c4e2bf7524a720cfbedc7f
cecyoursnikol <nikol.cecyours@gmail.com>
2024-12-10 17:01:17 +0530

    While Loop

---
+++ restartC/swithch-rev.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int main(){
+	
+	int num = 4;
+	
+	
+	switch(num){
+		
+		case 40:{
+			printf("The value is 40");
+			break;
+		}
+		
+		case 45:{
+			printf("The value is 45");
+			break;
+		}
+		
+		case 50:{
+			printf("The value is 50");
+			break;
+		}
+		default:{
+			
+			printf("The value is something else ");
+			break;
+		}
+	}
+	
+	
+	return 0;
+}
---
+++ restartC/whileLoop.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+	
+	int i,num = 7;
+	
+	
+//	starting the while
+
+while(i < num){
+	
+	printf("The value is %d\n",i);
+	i++;
+	
+	
+}
+	
+	
+	return 0;
+}
