2bd0d2c0c6cfa2e01730db859e7977fe6987b89f
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2024-12-19 00:50:20 -0800

    variable

--- C-2425/variables.c
+++ C-2425/variables.c
@@ -2,9 +2,22 @@
 
 int main(){
 	
+//	var declar
+	int dhruv  = 45;
 	
-	printf("HEllo Dhruv;");
+//	dhruv is identifier 
 	
+	
+	
+//	var printing
+	printf("%d\n",dhruv);
+	
+	
+//	updating variable
+
+	dhruv = 35;
+	
+	printf("The Updated Value of Dhruv is %d",dhruv);
 	
 	return 0;
 }
