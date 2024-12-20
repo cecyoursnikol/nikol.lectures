75daef46eb1db8858e142d092f8274a2525bb4cb
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2024-12-20 00:59:56 -0800

    Upload

---
+++ C-2425/dataTypes.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main(){
+	
+	
+//	interger
+	int a = 34;
+
+//Float
+	float f = 56.45;
+	
+	
+//	Double
+
+	double d = 23.23;
+	
+//	char 
+	char n = 'A';
+
+	printf("The Value of int is %i",a);
+	
+	printf("\nThe Value of Float is %.2f",f);
+	
+	printf("\nThe value od Double is %lf",d);
+	
+	printf("\nThe Value of Chat is %c",n);
+	
+	
+	
+	return 0;
+}
