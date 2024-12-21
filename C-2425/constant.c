971f29634035504b9887df6bd826eff6e24b7fdc
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2024-12-21 00:53:14 -0800

    Const

---
+++ C-2425/constant.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+	
+	int num = 60;
+	const int age = 67;
+	
+	
+//	updating a normal Var
+	num = 160;
+	
+	printf("The value is %d",num);
+	
+	
+//	printing the Constant 
+
+ 	age = 78;
+	printf("\nThe age is %i",age);
+	
+	return 0;
+}
