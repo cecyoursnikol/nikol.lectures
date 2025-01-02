3b7b395958164fca37582e64e23c76e0f1ff83ae
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2025-01-02 01:36:11 -0800

    switch Case

---
+++ C-2425/swithc-case.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+
+int main(){
+	
+	int day;
+	
+	
+	printf("Enter the day of this week");
+	scanf("%d",&day);
+	
+	
+	switch(day){
+		
+		
+		case 1:{
+			
+			printf("Sunday");
+			break;
+		}
+		case 2:{
+			printf("Monday");
+			break;
+		}
+		case 3:{
+			
+			printf("Tuesday");
+			break;
+		}
+		case 4:{
+			
+			printf("Wednesday");
+			break;
+		}
+		case 5:{
+			
+			printf("Thursday");
+			break;
+		}
+		case 6:{
+			
+			printf("Friday");
+			break;
+		}
+		case 7:{
+			
+			printf("Saturday");
+			break;
+		}
+		
+		default:{
+		printf("plz enter the number of day only!!!");
+			break;
+		}
+	}
+	
+	
+	return 0;
+}
