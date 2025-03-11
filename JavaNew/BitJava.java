82ef36ac58f6fdfa8263a42f4a28eda9c764122c
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2025-03-11 15:35:23 -0400

    Check

---
+++ JavaNew/BitJava.java
@@ -0,0 +1,22 @@
+public class BitJava{
+
+    public static void main(String[] args){
+
+        // System.out.println("Hello I am java");
+        int nuM = 10;
+        int B = 6;
+
+        // System.out.println(nuM);
+
+        // System.out.println(B);
+
+        System.out.println("The | opr is " + (nuM|B));
+
+        System.out.println("The & opr is " + (nuM&B));
+
+        System.out.println("The | opr is " + (nuM^B));
+
+
+
+    }
+}
