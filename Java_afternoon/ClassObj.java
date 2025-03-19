12fb73d96b89444c168ed0edb95a63963713ac47
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2025-03-19 15:28:16 -0400

    Upload

---
+++ Java_afternoon/ClassObj.java
@@ -0,0 +1,22 @@
+public class ClassObj {
+
+
+// Declaring a method
+    public void number(){
+
+        int a = 20;
+
+        System.out.println(a);
+    }
+
+    public static void main(String[] args) {
+      
+        
+        ClassObj Obj = new ClassObj();
+
+        Obj.number();
+
+
+    } 
+
+}
