f87d7ef1f23ece110b9a5f41ed43cb803e379e58
cecyoursnikol <139221301+cecyoursnikol@users.noreply.github.com>
2025-03-20 15:36:05 -0400

    Check Constructor

---
+++ Java_afternoon/ConstructorJava.java
@@ -0,0 +1,20 @@
+public class ConstructorJava {
+    
+    // Creat constructor
+    //Parametised constructor
+    public ConstructorJava(String Name){
+
+        // Name = "Hardi";
+
+        System.out.println(Name);
+
+    }
+
+
+    public static void main(String[] args) {
+        
+
+        ConstructorJava Onj = new ConstructorJava("Xyz");
+
+    }
+}
